d5bc732b95023a7b38e4888f02b63ccda68d320c
DueyDu <msdu1@asu.edu>
2017-04-03 11:31:55 -0700

    Update Change v1

--- ActivitySensor/ActivitySensor.ino
+++ ActivitySensor/ActivitySensor.ino
@@ -5,7 +5,8 @@ int xVolt;
 int yVolt;
 int zVolt;
 long tACC;
-void checkACC
+String accelData;
+int checkACC
 {
     //globalAccelFileName = "globalAccel" + String(globalAccelFileTracker) + ".txt";
     //globalAccelFile.open(globalAccelFileName, O_RDWR | O_CREAT | O_AT_END);
@@ -13,23 +14,23 @@ void checkACC
     int x = analogRead(A0);//3D movement
     int y = analogRead(A1);
     int z = analogRead(A2);
+    tACC = micros();
     int xVolt = x * 0.000805664;
     int yVolt = y * 0.000805664;
     int zVolt = z * 0.000805664;
-    tACC = micros();
     
+    //accelData += "X-Dimension: " + String(xVolt) + " Y-Dimension: " + String(yVolt) + " Z-Dimension: " + String(zVolt) +  " " + tAcc;
     //double accelMagnitude = floor(sqrt(pow(analogRead(A0),2) + pow(analogRead(A1),2) + pow(analogRead(A2),2)) + 0.5); 
     //accelData += String(accelMagnitude).remove(4); 
     
     // maximum String length is 622 bytes. Let's just be safe and say 616 so we don't overrun. 
     if (accelData.length() > 616) {
-        accelData += " " + Time.timeStr(); 
         //globalAccelFile.write(accelData);
         accelData = "";
     }
     
     if (accelData.length() <= 616) { 
-        accelData += " X-Dimension: " + String(xVolt) + " Y-Dimension: " + String(yVolt) + " Z-Dimension: " + String(zVolt) +  " " + tAcc "\n"; // each data point is on its own line
+        accelData += "X-Dimension: " + String(xVolt) + " Y-Dimension: " + String(yVolt) + " Z-Dimension: " + String(zVolt) +  " " + tAcc "\n"; // each data point is on its own line
     }
     
    // if (globalAccelFile.fileSize() > 1000000) // if the accel file exceeds 1MB, on to the next one
